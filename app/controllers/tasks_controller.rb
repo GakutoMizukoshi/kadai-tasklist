5eafc33d3a82452876e12e6ea0247e926a22c159
Gakuto mizukoshi <kiniro.kiniro@gmail.com>
2017-09-16 18:08:47 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
     
     def index
       @tasks = Task.all.page(params[:page]).per(5)
@@ -13,12 +15,12 @@ class TasksController < ApplicationController
     end
     
     def create
-      @task = Task.new(task_params)
-      
+      @task = current_user.tasks.build(task_params)
       if @task.save
         flash[:success] = "タスクが正常に投稿されました"
         redirect_to @task
       else
+        @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
         flash.now[:danger] = "タスクが投稿されませんでした" 
         render :new
       end  
@@ -41,7 +43,7 @@ class TasksController < ApplicationController
       @task.destroy
       
       flash[:success] = "タスクは正常に削除されました"
-      redirect_to tasks_url
+      redirect_back(fallback_location: root_path)
     end
     
     private
@@ -53,4 +55,12 @@ class TasksController < ApplicationController
     def task_params
       params.require(:task).permit(:content, :status)
     end  
+    
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      unless @micropost
+        redirect_to root_url
+      end
+    end
+
 end
