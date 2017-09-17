a3ab3a17ca3abf46f5383c292324f930d0af841a
Gakuto mizukoshi <kiniro.kiniro@gmail.com>
2017-09-17 07:52:02 +0000

    correction

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
     before_action :require_user_logged_in
-    before_action :correct_user, only: [:destroy]
+    before_action :correct_user, only: [:destroy, :update, :edit]
     
     def index
       @tasks = Task.all.page(params[:page]).per(5)
@@ -57,8 +57,8 @@ class TasksController < ApplicationController
     end  
     
     def correct_user
-      @micropost = current_user.microposts.find_by(id: params[:id])
-      unless @micropost
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
         redirect_to root_url
       end
     end
